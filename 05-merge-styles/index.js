ac7f09f84ab10c406dbe1cb734a75f56d866f90e
farsenyev <farsenyev@bk.ru>
2024-01-22 23:39:31 +0300

    feat: done 05 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('node:path')
+
+const stylePath = path.join(__dirname, 'styles')
+const projectPath = path.join(__dirname, 'project-dist')
+
+fs.readdir(stylePath, (err, files)=>{
+  if (err){console.log(err)}
+  else{
+    const styles = []
+    let counter = 0
+    files.forEach(file => {
+      fs.stat(stylePath + `/${file}`, (err, stats) => {
+        if (stats.isFile() && path.extname(file).toLowerCase() === '.css'){
+          counter ++
+          fs.readFile(stylePath + `/${file}`,'utf8', (err, data)=>{
+            if (err) {console.log(err)}
+            else{
+              styles.push(data)
+              if (styles.length === counter){
+                styles.forEach(style => {
+                  fs.appendFile(`${projectPath}/bundle.css`, style, 'utf8', (err)=>{
+                    if (err) console.log(err)
+                  })
+                })
+              }
+            }
+          })
+        }
+      })
+    })
+  }
+})
+
